6e2b6a5688c2903e49a9f6e26277b067dedd920f
Lorena-Concha <kleyna@gmail.com>
2022-03-06 16:47:41 -0300

    bono sensei

--- React/formulariocore/src/Components/main.jsx
+++ React/formulariocore/src/Components/main.jsx
@@ -13,31 +13,31 @@ const PersonCard = props => {
             <div className='formulario'>
             <label>First Name:<input type="text" name="Fname" className='input' placeholder='Primer Nombre' onChange={ (e) => setName(e.target.value) }/></label></div>
             <div className='validar'>
-            {name.length < 2 ? <p className='valmessage'>Nombre invalido</p> : ''}
+            {name.length > 0 && name.length < 2 ? <p className='valmessage'>Nombre invalido</p> : ''}
             </div>
             <div className='formulario'>
             <label>Last Name:<input type="text" name="Lname" className='input' placeholder='Apellido' onChange={ (e) => setLname(e.target.value) } /></label>
             </div>
             <div className='validar'>
-            {lname.length < 2 ? <p className='valmessage'>Apellido invalido</p> : ''}
+            {lname.length > 0 && lname.length < 2 ? <p className='valmessage'>Apellido invalido</p> : ''}
             </div>
             <div className='formulario'>
             <label>E-mail:<input type="text" name="mail" className='input' placeholder='correo electrónico' onChange={ (e) => setMail(e.target.value) }/></label>
             </div>
             <div className='validar'>
-            {mail.length < 5 ? <p className='valmessage'>mail invalido</p> : ''}
+            {mail.length > 0 && mail.length < 5 ? <p className='valmessage'>mail invalido</p> : ''}
             </div>
             <div className='formulario'>
             <label>Password:<input type="password" name="password" className='input'placeholder='Clave' onChange={ (e) => setPassword(e.target.value) }/></label>
             </div>
             <div className='validar'>
-            {password.length < 8 ? <p className='valmessage'>password invalido</p> : ''}
+            {password.length > 0 && password.length < 8 ? <p className='valmessage'>password invalido</p> : ''}
             </div>
             <div className='formulario'>
             <label>Confirm Password:<input type="password" name="cpassword" className='input' placeholder='confirma tu clave' onChange={ (e) => setCpassword(e.target.value) }/></label>
             </div>
             <div className='validar'>
-            {cpassword.length < 8 ? <p className='valmessage'>password invalido</p> : ''}
+            {cpassword.length > 0 && cpassword.length < 8 ? <p className='valmessage'>password invalido</p> : ''}
             </div>
             <div className='validar'>
             {password != cpassword? <p className='valmessage'>Las Contraseñas No coinciden</p> : ''}
